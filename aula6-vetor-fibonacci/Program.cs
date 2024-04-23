0dc37c1b33253fd02855073594843109c923f1a9
JulianaMistron <jumistron@yahoo.com.br>
2024-04-22 22:53:31 -0300

    Add project files.

---
+++ aula6-vetor-fibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿//faça um programa que leia um numero inteiro e imprima até esta posição da sequencia fibonacci.
+//nao precisa gravar a sequencia fibonaci, só imprimir
+
+
+int primeiroNumero = 1;
+int proximoNumero = 0;
+
+Console.Write("Digite a posição: ");
+int posicao = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < posicao; i++)
+{
+    Console.Write(primeiroNumero + " ");
+    primeiroNumero = primeiroNumero + proximoNumero;
+    proximoNumero = primeiroNumero - proximoNumero;
+}
